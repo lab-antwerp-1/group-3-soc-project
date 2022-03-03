d72385a89ed9de7f6d59ee553a62e639d97e7a27
denrique-alvarez <denrique.alvarez@gmail.com>
2022-03-03 14:55:40 +0100

    From scratch

--- client/src/components/render.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable folders/match-regex */
-export const renderList = (pending = []) => {
-  const pendingList = document.getElementById('pending-items-ul');
-  const liEl = document.createElement('li');
-  const element = pending[pending.length - 1];
-  liEl.innerHTML = element;
-  pendingList.appendChild(liEl);
-};
